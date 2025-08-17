2779d9c98032f012eefb807d6564e9eecc3e8aed
zthinhddo <ddthinh.dev@gmail.com>
2025-08-17 18:52:09 +0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://zthinhddo.github.io/e-plantShopping/",
+  base: "/e-plantShopping/",
   plugins: [react()],
 })
